5bc82d771a7e16afc2ffef8b90d99dd2af777c56
MrElgheim <petter.elgheim@gmail.com>
2021-08-23 14:48:41 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("teller til ti");
+        for (int i = 0; i<10; ++i) {
+            System.out.println(i);
+        }
+        System.out.println("Hva med deg Ragnsan, kan du telle?");
+
+        System.out.println("Få se deg telle Enock!");
+    }
+}
